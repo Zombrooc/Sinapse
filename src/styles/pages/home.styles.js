68cad7c679c8d98d652cf6e162b055aac740d7b0
Elian Valdez <elian.valdez09@gmail.com>
2021-09-03 22:43:02 -0300

    a

--- src/styles/pages/home.styles.js
+++ src/styles/pages/home.styles.js
@@ -48,12 +48,12 @@ export const GridContainer = styled.div`
       }
     }
 
-    /* .right-container {
+    .right-container {
       img {
         width: 100%;
         height: 100%;
       }
-    } */
+    }
 
     /* .right-container {
       background-position: center;
