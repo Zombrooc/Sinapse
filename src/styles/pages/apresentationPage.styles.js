a4c9f24892cd83d7bc380a37fee93b61cf3a63a8
Elian Valdez <elian.valdez09@gmail.com>
2021-09-04 22:15:10 -0300

    saushausghas

--- src/styles/pages/apresentationPage.styles.js
+++ src/styles/pages/apresentationPage.styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100vw;
-  /* height: 100vh; */
+  height: 100vh;
   background-attachment: fixed;
   background-position: center;
   background-repeat: no-repeat;
