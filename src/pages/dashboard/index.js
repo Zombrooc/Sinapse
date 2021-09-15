ac1ac57bdf8128e59031826bdfce07c83788350a
Elian Valdez <elian.valdez09@gmail.com>
2021-09-15 10:20:10 -0300

    Dashboard auto redirect added

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -35,25 +35,7 @@ export default function Dashboard() {
       </Container>
     );
   } else {
-    return (
-      <Container>
-        <Head>
-          <title> Sinapse Dashboard </title>
-        </Head>
-
-        <CenterBox>
-          <div className="f-modal-alert">
-            <div className="f-modal-icon f-modal-warning scaleWarning">
-              <span className="f-modal-body pulseWarningIns"></span>
-              <span className="f-modal-dot pulseWarningIns"></span>
-            </div>
-          </div>
-          <p>Efetue Login para poder acessar essa página.</p>
-          <br />
-          <button onClick={() => signIn()}>Entrar</button>
-        </CenterBox>
-      </Container>
-    );
+    return signIn();
   }
 }
 
