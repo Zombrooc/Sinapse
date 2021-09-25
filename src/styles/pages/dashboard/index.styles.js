4c0b99558781b788c61ff85ac6522bad7d740369
Elian Valdez <elian.valdez09@gmail.com>
2021-09-24 21:50:33 -0300

    a

--- src/styles/pages/dashboard/index.styles.js
+++ src/styles/pages/dashboard/index.styles.js
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  padding: 15px;
+  /* padding: 15px; */
+  height: 100vh;
 
   grid-template-columns: 1fr 1fr;
   background-attachment: fixed;
   background-position: center;
-  background-repeat: no-repeat;
+  background-repeat: no-repeat !important;
   background-size: cover;
 
   .grid {
@@ -78,6 +79,6 @@ export const CardButton = styled.div`
     box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px,
       rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
 
-      filter: brightness(85%);
+    filter: brightness(85%);
   }
 `;
